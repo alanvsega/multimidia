3e69b81005b5ef10ae4c2853812450389dbbb094
Campinas - Rafael Gurtler Bekedorf <i405962@fb.org.br>
2024-05-28 21:12:09 -0300

    aulas de data binding, diretivas e eventos

---
+++ aula1/index.js
@@ -0,0 +1,6 @@
+function imprimeNome() {
+    let nome = document.getElementById("nome").value
+
+    let h4nome = document.getElementById("imprimeAqui")
+    h4nome.textContent = nome
+}
---
+++ vue/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Olá {{ name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    name: 'Alan'
+  })
+}
+</script>
+
+<style>
+
+</style>
